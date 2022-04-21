52e311fbdc0e1202f038db29ba3b884db0dfcb56
andersonxss <andersonxss@hotmail.com>
2022-04-21 09:19:58 -0300

    Entitie Order

---
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/http/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateCustomerService {
+  public async execute({ id, name, email }: IRequest): Promise<Customer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const customer = await customersRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    const customerUpdateEmail = await customersRepository.findByEmail(email);
+    if (customerUpdateEmail && customerUpdateEmail.email !== email) {
+      throw new AppError('There is already one customer with this email.');
+    }
+
+    customer.name = name;
+    customer.email = email;
+    await customersRepository.save(customer);
+    return customer;
+  }
+}
+
+export default UpdateCustomerService;
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -3,6 +3,8 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profiledRouter from '@modules/users/routes/profiles.routes';
+import customerRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
@@ -10,5 +12,7 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profiledRouter);
+routes.use('/customers', customerRouter);
 
 export default routes;
